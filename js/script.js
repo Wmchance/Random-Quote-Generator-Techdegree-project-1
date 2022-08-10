dfa04fdb7e322e8c85fc46b5b521fa6effb39121
Will <wmchance@gmail.com>
2022-08-10 11:58:00 +0700

    Added quotes array with quote data

--- js/script.js
+++ js/script.js
@@ -11,7 +11,46 @@ project 1 - A Random Quote Generator
  * `quotes` array 
 ***/
 
+const quotes = [
 
+    {
+      quote: "Well, I've made up my mind, anyway. I want to see mountains again...",
+      source: "Bilbo Baggins",
+      citation: "The Lord of the Rings",
+      year: "October 20th 1955",
+    },
+
+    {
+      quote: "All the money you made will never buy back your soul",
+      source: "Bob Dylan",
+      citation: "The Freewheelin’ Bob Dylan",
+      year: "1963",
+    },
+
+    {
+      quote: "Mellow is the man who knows what he's been missing.",
+      source: "Led Zeppelin",
+      citation: "Over the Hills and Far Away",
+      year: "1973",
+    },
+
+    {
+      quote: "All this happened, more or less.",
+      source: "Kurt Vonnegut",
+      citation: "Slaughterhouse-Five",
+      year: "1969",
+    },
+
+    {
+      quote: "I believe in absolute honesty and sensible social lies.",
+      source: "Samantha Black Crow",
+      citation: "American Gods",
+      year: "2001",
+    },
+
+]
+
+//console.log(quotes[0].quote);
 
 /***
  * `getRandomQuote` function
