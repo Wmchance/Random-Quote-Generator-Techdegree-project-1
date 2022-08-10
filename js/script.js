9145bd26314bfdd34ab81fc04fa9026a7c4d29cc
Will <wmchance@gmail.com>
2022-08-10 14:15:21 +0700

    Add comments to code

--- js/script.js
+++ js/script.js
@@ -9,6 +9,9 @@ project 1 - A Random Quote Generator
 
 /*** 
  * `quotes` array 
+ * Quote data is stored here. 
+ * The data is stored in an array of objects
+ * The properties in each object are: quote, source, citation, year
 ***/
 
 const quotes = [
@@ -17,7 +20,7 @@ const quotes = [
       quote: "Well, I've made up my mind, anyway. I want to see mountains again...",
       source: "Bilbo Baggins",
       citation: "The Lord of the Rings",
-      year: "October 20th 1955",
+      year: "1955",
     },
 
     {
@@ -28,21 +31,21 @@ const quotes = [
     },
 
     {
-      quote: "Mellow is the man who knows what he's been missing.",
+      quote: "Mellow is the man who knows what he's been missing",
       source: "Led Zeppelin",
       citation: "Over the Hills and Far Away",
       year: "1973",
     },
 
     {
-      quote: "All this happened, more or less.",
+      quote: "All this happened, more or less",
       source: "Kurt Vonnegut",
       citation: "Slaughterhouse-Five",
       year: "1969",
     },
 
     {
-      quote: "I believe in absolute honesty and sensible social lies.",
+      quote: "I believe in absolute honesty and sensible social lies",
       source: "Samantha Black Crow",
       citation: "American Gods",
       year: "2001",
@@ -52,17 +55,22 @@ const quotes = [
 
 /***
  * `getRandomQuote` function
+ * This function has two steps.
+ * First, it generates a random number between 0 and the final index of the 'quotes' array
+ * Second, it uses the generated number to select and return the object from the 'quotes' array whose index corresponds to the random number
 ***/
 
 const getRandomQuote = function () {
 
-  const randomNum = Math.floor(Math.random() * quotes.length);
+  const randomNum = Math.floor(Math.random() * quotes.length); //generate a random number
   return quotes[randomNum];
   
 }
 
 /***
  * `printQuote` function
+ * First, this function builds a string which contains the property values from the selected quote object
+ * Second, it uses '.innerHTML' to display the constructed string to the user
 ***/
 
 const printQuote = function () {
@@ -76,12 +84,14 @@ const printQuote = function () {
   
   `;
 
+  //Checks if the 'citation' property exists, and adds it's value to the string if so
   if (randomQuote.citation != undefined) {
 
       HTML += `<span class="citation"> ${randomQuote.citation} </span>`;
 
   }
 
+  //Checks if the 'year' property exists, and adds it's value to the string if so
   if (randomQuote.year != undefined) {
 
     HTML += `<span class="year"> ${randomQuote.year} </span>`;
@@ -90,6 +100,7 @@ const printQuote = function () {
 
   HTML += `</p>`;
 
+  //display constructed string to user
   document.getElementById('quote-box').innerHTML = HTML;
 
 }
